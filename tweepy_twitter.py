4929299fe3cf0f16003110ac8ef53fe2945d804a
sb-haz <sb_haz@icloud.com>
2021-12-04 21:44:36 +0000

    Generate word clouds

--- tweepy_twitter.py
+++ tweepy_twitter.py
@@ -1,16 +1,20 @@
-import tweepy
-import re
-import textblob
-#from textblob import TextBlob
-import twitter_credentials
+import tweepy  # tweepy
+import re  # regex
+import twitter_credentials  # api keys
+import os  # for os file
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator  # wordcloud
+from PIL import Image  # load image
+import numpy as np  # get colour of image
+
+import sys # running python file with args, remove later
 
 
 # Client
 def getClient():
-    client = tweepy.Client(bearer_token = twitter_credentials.bearer_token,
-                           consumer_key = twitter_credentials.consumer_key,
-                           consumer_secret = twitter_credentials.consumer_secret,
-                           access_token = None, access_token_secret = None)
+    client = tweepy.Client(bearer_token=twitter_credentials.bearer_token,
+                           consumer_key=twitter_credentials.consumer_key,
+                           consumer_secret=twitter_credentials.consumer_secret,
+                           access_token=None, access_token_secret=None)
     return client
 
 
@@ -24,10 +28,10 @@ def getUserInfo(user):
 # Tweets
 def getUserRecentTweets(id):
     client = getClient()
-    user_tweets = client.get_users_tweets(id = id,
-                                          tweet_fields = ['public_metrics'],
-                                          exclude = ['retweets','replies'],
-                                          max_results = 100,
+    user_tweets = client.get_users_tweets(id=id,
+                                          tweet_fields=['public_metrics'],
+                                          exclude=['retweets', 'replies'],
+                                          max_results=100,
                                           #start_time = '2021-09-02T00:00:00.000Z'
                                           )
     return user_tweets
@@ -35,20 +39,84 @@ def getUserRecentTweets(id):
 
 # Get user recent tweets
 def storeUserTweets(username):
-    user = getUserInfo(username) # get user info, such as id
-    user_tweets = getUserRecentTweets(user.id) # get tweets of user by id
+    user = getUserInfo(username)  # get user info, such as id
+    user_tweets = getUserRecentTweets(user.id)  # get tweets of user by id
+
+    file_path = 'user_tweets/' + username + '.txt'
     
+    # user has tweets
     if len(user_tweets.data) > 0:
-        for x in user_tweets.data:
-            print(clean_tweet(str(x)))
+        
+        # user tweets not stored in file
+        if not os.path.exists(file_path):
+            
+            # create new file
+            file = open(file_path, 'w')
+                
+            # write each tweet into new file
+            for x in user_tweets.data:
+                file.write(clean_tweet(str(x)) + '\n')
+
+            file.close()
+            
+        else:
+            # return as error in future
+            print("User tweets file already exists")
     else:
+        # user has no tweets
         print("No tweets")
-            
-            
+
+
 # credit freeCodeCamp.org
 # removes special characters and hyperlinks
-# \' escapes apostrophes
+# ’' escapes apostrophes
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z’' \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-storeUserTweets('tweetwrapped')
+
+# Generating word cloud image
+def createUserWordCloud(username):
+    # Content-related
+    text = open('user_tweets\\' + username + '.txt',
+                'r', encoding='utf-8').read()
+    stopwords = STOPWORDS
+
+    # Mask
+    custom_mask = np.array(Image.open('masks\\twitter_logo.png'))
+    font = 'fonts\\SFProDisplay-Light.ttf'
+
+    # WordCloud attributes
+    wordCloud = WordCloud(
+        font_path=font,
+        #margin = 10,
+        mask=custom_mask,
+        background_color='black',
+        #background_color = None,
+        #mode = 'RGBA',
+        stopwords=stopwords,
+        height=1000,
+        width=1000,
+        include_numbers=False,  # include numbers
+        # color_func = lambda *args, **kwargs: (255,255,255) # text colour
+    )
+    
+    # Generate
+    wordCloud.generate(text)
+
+    # Use colour of mask image
+    ## image_colours = ImageColorGenerator(custom_mask)
+    ## wordCloud.recolor(color_func = image_colours)
+
+    # Store to file
+    wordCloud.to_file('word_cloud_output\\' + username + '.png')
+
+
+def main(username):
+    # Get user tweets
+    # Create word cloud
+    storeUserTweets(username)
+    createUserWordCloud(username)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
