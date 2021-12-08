64b8b5803743ca2b3a27246b4179902e4d7eeea2
sb-haz <sb_haz@icloud.com>
2021-12-08 20:13:53 +0000

    refactor

--- tweepy_twitter.py
+++ tweepy_twitter.py
@@ -1,3 +1,4 @@
+from PIL import Image,ImageFont,ImageDraw
 import tweepy  # tweepy
 import re  # regex
 import twitter_credentials  # api keys
@@ -6,7 +7,7 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator  # wordcloud
 from PIL import Image  # load image
 import numpy as np  # numerical python library
 import pandas as pd  # store content into dataframes
-from textblob import TextBlob # sentiment analysis
+from textblob import TextBlob  # sentiment analysis
 
 import sys  # running python file with args, remove later
 
@@ -30,26 +31,24 @@ def getUserInfo(user):
 # Tweets
 def getUserRecentTweets(id):
     client = getClient()
-    user_tweets = client.get_users_tweets(id = id,
-                                          tweet_fields = ['public_metrics,created_at'],
-                                          exclude = ['retweets', 'replies'],
-                                          max_results = 100,
+    user_tweets = client.get_users_tweets(id=id,
+                                          tweet_fields=['public_metrics,created_at'],
+                                          exclude=['retweets', 'replies'],
+                                          max_results=100,
                                           #start_time = '2021-09-02T00:00:00.000Z'
                                           )
-    #print(user_tweets.data[0].public_metrics)
+    
     return user_tweets
 
 
 # Get user recent tweets
-def storeUserTweets(username):
-    user = getUserInfo(username)  # get user info, such as id
-    user_tweets = getUserRecentTweets(user.id)  # get tweets of user by id
+def storeUserTweets(username, user_tweets):
 
     file_path = 'user_tweets/' + username + '.txt'
 
     # user has tweets
     if len(user_tweets.data) > 0:
-        
+
         # user tweets not stored in file
         if not os.path.exists(file_path):
 
@@ -67,7 +66,7 @@ def storeUserTweets(username):
             # return as error in future
             # print("User tweets file already exists")
             return False
-        
+
     else:
         # user has no tweets
         # print("No tweets")
@@ -79,8 +78,10 @@ def cleanTweet(tweet):
     # replace ’ with '
     tweet = tweet.replace('’', '\'')
 
-    # credit freeCodeCamp.org - removes special characters and hyperlinks
-    # modified to allow '
+    """
+    credit freeCodeCamp.org - removes special characters and hyperlinks
+    modified to allow '
+    """
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z' \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 
@@ -100,16 +101,16 @@ def createUserWordCloud(username):
 
     # WordCloud attributes
     wordCloud = WordCloud(
-        font_path = font,
+        font_path=font,
         #margin = 10,
-        mask = custom_mask,
-        background_color = 'black',
+        mask=custom_mask,
+        background_color='black',
         #background_color = None,
         #mode = 'RGBA',
-        stopwords = stopwords,
-        height = 1000,
-        width = 1000,
-        include_numbers = False,  # include numbers
+        stopwords=stopwords,
+        height=1000,
+        width=1000,
+        include_numbers=False,  # include numbers
         # color_func = lambda *args, **kwargs: (255,255,255) # text colour
     )
 
@@ -121,65 +122,102 @@ def createUserWordCloud(username):
     ## wordCloud.recolor(color_func = image_colours)
 
     # Store to file
-    wordCloud.to_file('word_cloud_output\\' + username + '.png')
+    wordCloud.to_file('img\\outputs\\word_cloud\\' + username + '.png')
 
-
-def main(username):
-
-    # Get user tweets
-    # Create word cloud
-    if storeUserTweets(username):
-        createUserWordCloud(username)
-
+# Add public metrics to dataframe
 def tweetsToDataFrame(tweets):
-    df = pd.DataFrame(data = [tweet.text for tweet in tweets], columns=['tweets'])
-    
-    #df['retweet_count'] = np.array([tweet.public_metrics.get('retweet_count') for tweet in tweets])
-    #df['reply_count'] = np.array([tweet.public_metrics.get('reply_count') for tweet in tweets])
-    #df['like_count'] = np.array([tweet.public_metrics.get('like_count') for tweet in tweets])
-    #df['quote_count'] = np.array([tweet.public_metrics.get('quote_count') for tweet in tweets])
-    #df['created_at'] = np.array([tweet.created_at for tweet in tweets])
-    
+    df = pd.DataFrame(
+        data=[tweet.text for tweet in tweets], columns=['tweets'])
+
+    df['retweet_count'] = np.array(
+        [tweet.public_metrics.get('retweet_count') for tweet in tweets])
+    df['reply_count'] = np.array(
+        [tweet.public_metrics.get('reply_count') for tweet in tweets])
+    df['like_count'] = np.array(
+        [tweet.public_metrics.get('like_count') for tweet in tweets])
+    df['quote_count'] = np.array(
+        [tweet.public_metrics.get('quote_count') for tweet in tweets])
+    df['created_at'] = np.array([tweet.created_at for tweet in tweets])
+
     return df
 
 
+"""
+ Sentiment analysis, returns 
+ -1 for negative
+ 0 for neutral
+ 1 for positive
+"""
 def analyse_sentiment(tweet):
     analysis = TextBlob(cleanTweet(tweet))
-    return analysis.sentiment.polarity
     
-    # return polarity as 1, 0, -1
-    # if analysis.sentiment.polarity > 0:
-    #     return 1
-    # elif analysis.sentiment.polarity == 0:
-    #     return 0
-    # else:
-    #     return -1
-    
-    
-def main2(username):
-    user = getUserInfo(username)  # get user info, such as id
-    user_tweets = getUserRecentTweets(user.id)
-    
-    #print(dir(user_tweets.data)) # What attributes exist
-    #print(user_tweets.data[0].public_metrics)
-    
-    df = tweetsToDataFrame(user_tweets.data)
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
 
+
+def main(username):
+    user = getUserInfo(username)  # get user info, such as id
+    user_tweets = getUserRecentTweets(user.id)  # get tweets of user by id
+
+    # get user tweets
+    # create word cloud
+    if storeUserTweets(username, user_tweets):
+        createUserWordCloud(username)
+
+    # get user stats
+    df = tweetsToDataFrame(user_tweets.data)
+    
+    # carry out sentiment analysis
+    df['sentiment'] = np.array([analyse_sentiment(tweet) for tweet in df['tweets']])
+    
+    # change how many rows df prints
+    #pd.set_option('display.max_rows', 100)
+    #print(df.head(100))  # print dataframe
+    # 
+    # print(dir(user_tweets.data)) # What attributes exist
+    # print(user_tweets.data[0].public_metrics)
+    #
     # print metrics
     #print('retweets ', np.max(df['retweet_count']))
     #print('reply ', np.max(df['reply_count']))
     #print('like ', np.max(df['like_count']))
     #print('quote ', np.max(df['quote_count']))
     #print('created_at ', np.max(df['created_at']))
-    
-    df['sentiment'] = np.array([analyse_sentiment(tweet) for tweet in df['tweets']])
-    pd.set_option('display.max_rows', 100)
 
-    print(df.head(100)) # print dataframe
-    
-    print('Overall sentiment ', np.average(df['sentiment']) * 100)
+    # print average sentiment
+    print(np.average(df['sentiment']) * 100)
+    # sentiment to happiness
+    sentiment = np.average(df['sentiment']) * 100
+    sentiment_image(sentiment)
+   
+        
+def sentiment_image(sentiment):
+    if sentiment > 10:
+        sentiment_class = "VERY HAPPY!"
+    elif sentiment > 5:
+        sentiment_class = "HAPPY"
+    elif sentiment > 0:
+        sentiment_class = "BARELY HAPPY"
+    elif sentiment > -5:
+        sentiment_class = "DOWN BAD"
+    else:
+        sentiment_class = "DOWN TERRIBLE"
 
+    img = Image.open("white.png")
+    font = ImageFont.truetype("arial.ttf", 24)
+    
+    draw = ImageDraw.Draw(img)
+    
+    text = "Hello World"
+    
+    draw.text((0,150), text, (0,0,0), font=font)
+    img.save("test.png")
+    
     
 if __name__ == "__main__":
-    main2('mbbants1')
-    #main(sys.argv[1])
+    #main2('twitter')
+    main(sys.argv[1])
