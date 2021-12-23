4dff309a95b7092774d6694bb09b3da0f99bda46
sb-haz <sb_haz@icloud.com>
2021-12-23 02:49:06 +0000

    Finally fkn works

--- test_mentions.py
+++ test_mentions.py
@@ -20,12 +20,11 @@ class streamListener(tweepy.Stream):
         # can also do 'name' to get username
         # if 'protected' true, then stop
         # if 'following' false, then stop
-        
+
         tweet_id = clean_data['id']
-        tweet_username = clean_data['user']['screen_name'] # screen_name
-        tweet_text = "@" + tweet_username + " Here's your Twitter Wrapped 2021!"
-        
-        print(tweet_text) # @FinessTV Here's your Twitter Wrapped 2021!
+        tweet_username = clean_data['user']['screen_name']  # screen_name
+        tweet_text = "@" + tweet_username
+
         respondToTweet(tweet_text, tweet_id)
         return True
 
@@ -42,19 +41,21 @@ def setUpAuth():
 
 
 def followStream():
-    twitter_stream = streamListener(consumer_key, consumer_secret, access_token, access_token_secret)
+    twitter_stream = streamListener(
+        consumer_key, consumer_secret, access_token, access_token_secret)
     twitter_stream.filter(track=['@TweetWrapped hi'])
 
 
 def respondToTweet(tweet_text, tweet_id):
     #api = setUpAuth()
-    
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth=auth, retry_count=5)
-    
-    api.update_status(status=tweet_text, in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
-    # tweepy.errors.Unauthorized: 401 Unauthorized
+
+    api.update_status(status=tweet_text, in_reply_to_status_id=tweet_id,
+                      auto_populate_reply_metadata=True)
+
 
 if __name__ == "__main__":
     followStream()
