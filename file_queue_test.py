5614d6586d093578b7ec5ebeef72f5bf086fb74c
sb-haz <sb_haz@icloud.com>
2021-12-25 23:38:25 +0000

    Test file queue system

--- file_queue_test.py
+++ file_queue_test.py
@@ -1,23 +1,41 @@
 import json
 
 tweet1 = {
-    "username": "FinessTV",
+    "username": "a",
     "id": 234234
 }
 tweet2 = {
-    "username": "Talal",
+    "username": "b",
+    "id": 76766
+}
+tweet3 = {
+    "username": "c",
+    "id": 76766
+}
+tweet4 = {
+    "username": "d",
+    "id": 76766
+}
+tweet5 = {
+    "username": "e",
     "id": 76766
 }
 
-def file_queue_add():
-    with open('queue.txt', 'a') as file:
-        file.write(str(tweet1) + "\n")
-        file.write(str(tweet2) + "\n")
+# Add to queue
+def file_queue_add(tweet):
     
+    # Open file in append mode
+    with open('queue_test.txt', 'a') as file:
+        
+        # Write tweet to file
+        file.write(str(tweet) + "\n")
+    
+
+# Get then remove first line of queue
 def file_queue_pop():
     
     # Load queue file in read mode
-    with open('queue.txt', 'r') as file:
+    with open('queue_test.txt', 'r') as file:
                 
         # All file data
         data = file.read().splitlines(True)
@@ -25,17 +43,27 @@ def file_queue_pop():
         # Get first line
         # Replace ' with "
         # Json decoder requires "
-        first_line = data[0].replace("'", "\"")
+        # Remove \n
+        first_line = data[0].replace("'", "\"").replace("\n", "")
         
         # Convert to dictionary
         tweet = json.loads(first_line)
         
     # Load file again but in write mode
-    with open('queue.txt', 'w') as file:
+    with open('queue_test.txt', 'w') as file:
         
         # Write data to file without first line
         file.writelines(data[1:])
 
-    return tweet
+    print(tweet["username"])
 
-file_queue_pop()
+
+#file_queue_add(tweet1)
+#file_queue_add(tweet2)
+#file_queue_add(tweet3)
+#file_queue_add(tweet4)
+#file_queue_add(tweet5)
+try:
+    file_queue_pop()
+except Exception as e:
+    print(e)
